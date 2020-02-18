0abf95aee413862bc6b6a65c7fe005b756fcae2f
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-18 00:46:35 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -12,8 +12,8 @@ def assembled_aoh
   array = [
     JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" },
     PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
-KATE_G = { name:  "Kate Gompert", occupation: "None" }
-BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
+    KATE_G = { name:  "Kate Gompert", occupation: "None" },
+    BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
 end
 
